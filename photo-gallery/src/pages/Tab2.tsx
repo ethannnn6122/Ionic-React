d40b46db6bbabc348bee902eefb12d344e9ab4fd
Ethan Gruenemeier <EGruenemeier984@west-mec.org>
2020-03-22 00:21:56 -0700

    Completed Photo-Gallery
    Simple Ionic tutorial creating a photo gallery app that works on android, IOS, and web.

---
+++ photo-gallery/src/pages/Tab2.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { usePhotoGallery, Photo } from '../hooks/usePhotoGallery';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, 
+  IonFab, IonFabButton, IonIcon, IonGrid, IonRow, 
+  IonCol, IonImg, IonActionSheet } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+import { camera, trash, close } from 'ionicons/icons';
+
+
+  const Tab2: React.FC = () => {
+    const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+    const [photoToDelete, setPhotoToDelete] = useState<Photo>();
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg onClick={() => setPhotoToDelete(photo)} src={photo.base64 ?? photo.webviewPath} />
+              </IonCol>
+              ))}
+          </IonRow>
+        </IonGrid>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+        <IonActionSheet
+          isOpen={!!photoToDelete}
+          buttons={[{
+          text: 'Delete',
+          role: 'destructive',
+          icon: trash,
+          handler: () => {
+            if (photoToDelete) {
+              deletePhoto(photoToDelete);
+              setPhotoToDelete(undefined);
+            }
+          }
+        }, {
+        text: 'Cancel',
+        icon: close,
+        role: 'cancel'
+      }]}
+      onDidDismiss={() => setPhotoToDelete(undefined)}
+    />
+      </IonContent>
+    </IonPage>
+  );
+};
+export default Tab2;
